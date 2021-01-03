7245d4b95b0b35c9a324b7d3d490434e9a4be04d
oscar0812 <oscar0812torres@gmail.com>
2021-01-03 17:03:40 -0600

    tweaking pypi upload files

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Oscar R. Torres',  # Type in your name
     author_email='oscar0812torres@gmail.com',  # Type in your E-Mail
     url='https://github.com/oscar0812/pyfinviz',  # Provide either the link to your github or to your website
-    download_url='https://github.com/oscar0812/pyfinviz/archive/v_01.tar.gz',  # I explain this later on
+    download_url='https://github.com/oscar0812/pyfinviz/archive/v_02.tar.gz',  # I explain this later on
     keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'validators',
