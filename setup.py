81bf53ca713769db333ac93f0bce2d1f640a7743
oscar0812 <oscar0812torres@gmail.com>
2021-01-03 16:54:59 -0600

    release version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyfinviz',  # How you named your package folder (MyLib)
     packages=['pyfinviz'],  # Chose the same as "name"
-    version='0.1',  # Start with a small number and increase it with every change you make
+    version='0.2',  # Start with a small number and increase it with every change you make
     license='apache-2.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Scrape data from finviz.com',  # Give a short description about your library
     author='Oscar R. Torres',  # Type in your name
