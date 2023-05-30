2f76b96326011ba1f3ede24d2ef0f7d83c63b624
oscar0812 <oscar0812torres@gmail.com>
2023-05-29 22:34:25 -0500

    TESTS | offline quote tests

--- pyfinviz/test/test_quote.py
+++ pyfinviz/test/test_quote.py
@@ -1,10 +1,23 @@
 from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from bs4 import BeautifulSoup
+
 from pyfinviz.quote import Quote
 
 
 class TestQuote(TestCase):
+    @patch("pyfinviz.quote.BeautifulSoup")
+    def test__get_outer_ratings_df__TABLE_IS_NONE(self, mock_soup):
+        mock_soup.find.return_value = None
 
-    def test_main_AMZN(self):
+        assert Quote.__get_outer_ratings_df__(mock_soup) is None
+
+    @patch("pyfinviz.utils.WebScraper.get_soup")
+    def test_main_AMZN(self, mock_get_soup: Mock):
+        with open('html/quote_amzn.html', 'r') as f:
+            mock_get_soup.return_value = BeautifulSoup(f.read(), 'lxml')
+
         quote = Quote('AMZN')
 
         self.assertIsNotNone(quote)
@@ -14,14 +27,25 @@ class TestQuote(TestCase):
         self.assertEqual('Amazon.com, Inc.', quote.company_name)
         self.assertEqual(['Consumer Cyclical', 'Internet Retail', 'USA'], quote.sectors)
         self.assertEqual(71, quote.fundamental_df.columns.size)
+        self.assertEqual(1, len(quote.fundamental_df))
         self.assertEqual(5, quote.outer_ratings_df.columns.size)
+        self.assertEqual(20, len(quote.outer_ratings_df))
         self.assertEqual(4, quote.outer_news_df.columns.size)
+        self.assertEqual(100, len(quote.outer_news_df))
         self.assertEqual(20, quote.income_statement_df.columns.size)
+        self.assertEqual(8, len(quote.income_statement_df))
         self.assertEqual(11, quote.insider_trading_df.columns.size)
+        self.assertEqual(91, len(quote.insider_trading_df))
         self.assertEqual(37, quote.balance_sheet_df.columns.size)
+        self.assertEqual(8, len(quote.balance_sheet_df))
         self.assertEqual(23, quote.cash_flow_df.columns.size)
+        self.assertEqual(8, len(quote.cash_flow_df))
 
-    def test_main_META(self):
+    @patch("pyfinviz.utils.WebScraper.get_soup")
+    def test_main_META(self, mock_get_soup: Mock):
+        with open('html/quote_meta.html', 'r') as f:
+            mock_get_soup.return_value = BeautifulSoup(f.read(), 'lxml')
+
         quote = Quote('META')
 
         self.assertIsNotNone(quote)
@@ -31,14 +55,25 @@ class TestQuote(TestCase):
         self.assertEqual('Meta Platforms, Inc.', quote.company_name)
         self.assertEqual(['Communication Services', 'Internet Content & Information', 'USA'], quote.sectors)
         self.assertEqual(71, quote.fundamental_df.columns.size)
+        self.assertEqual(1, len(quote.fundamental_df))
         self.assertEqual(5, quote.outer_ratings_df.columns.size)
+        self.assertEqual(20, len(quote.outer_ratings_df))
         self.assertEqual(4, quote.outer_news_df.columns.size)
+        self.assertEqual(100, len(quote.outer_news_df))
         self.assertEqual(20, quote.income_statement_df.columns.size)
+        self.assertEqual(8, len(quote.income_statement_df))
         self.assertEqual(11, quote.insider_trading_df.columns.size)
+        self.assertEqual(100, len(quote.insider_trading_df))
         self.assertEqual(37, quote.balance_sheet_df.columns.size)
+        self.assertEqual(8, len(quote.balance_sheet_df))
         self.assertEqual(23, quote.cash_flow_df.columns.size)
+        self.assertEqual(8, len(quote.cash_flow_df))
 
-    def test_main_DOESNOTEXIST1(self):
+    @patch("pyfinviz.utils.WebScraper.get_soup")
+    def test_main_DOESNOTEXIST1(self, mock_get_soup: Mock):
+        with open('html/quote_doesnotexist1.html', 'r') as f:
+            mock_get_soup.return_value = BeautifulSoup(f.read(), 'lxml')
+
         quote = Quote('DOESNOTEXIST1')
 
         self.assertIsNotNone(quote)
